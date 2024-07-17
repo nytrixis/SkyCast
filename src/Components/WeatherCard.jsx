55d4e598a1a6b6ee12103a986daaa956678d15e5
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2024-07-17 23:56:48 +0530

    Wind Direction Update

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -24,6 +24,34 @@ const WeatherCard = ({
   const [icon, setIcon] = useState(sun)
   const {time} = useDate()
 
+  const getWindDirection = (deg) => {
+    if (deg > 337.5) {
+      return 'N'
+    }
+    if (deg > 292.5) {
+      return 'NW'
+    }
+    if (deg > 247.5) {
+      return 'W'
+    }
+    if (deg > 202.5) {
+      return 'SW'
+    }
+    if (deg > 157.5) {
+      return 'S'
+    }
+    if (deg > 122.5) {
+      return 'SE'
+    }
+    if (deg > 67.5) {
+      return 'E'
+    }
+    if (deg > 22.5) {
+      return 'NE'
+    }
+    return 'N'
+  }
+
   useEffect(() => {
     if (iconString){
       if(iconString.toLowerCase().includes('cloud')){
